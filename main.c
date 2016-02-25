e2925fc4f25acede31b5949ce488e63e4855dede
brandon kehl <kehl.brandon@gmail.com>
2016-02-24 19:28:12 -0600

    Update main.c

--- main.c
+++ main.c
@@ -3,7 +3,7 @@
 //  PBR
 //
 //  Created by brandon kehl on 7/23/15.
-//  Copyright (c) 2015 bignerdranch. All rights reserved.
+//  Copyright (c) 2015 kehldesigngroup. All rights reserved.
 //
 
 #include <stdio.h>
